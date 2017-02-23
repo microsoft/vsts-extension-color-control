9b6737ca93e8d8719b0cf60b77733d99385a81d8
Otto Streifel <ottost@microsoft.com>
2017-02-23 10:23:57 -0800

    Add ctr + s event listener

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,6 +1,17 @@
 /// <reference path="../typings/index.d.ts" />
 import { Controller } from "./control";
 import * as ExtensionContracts from "TFS/WorkItemTracking/ExtensionContracts";
+import { WorkItemFormService } from "TFS/WorkItemTracking/Services";
+
+// save on ctr + s
+$(window).bind("keydown", function (event: JQueryEventObject) {
+    if (event.ctrlKey || event.metaKey) {
+        if (String.fromCharCode(event.which) === "S") {
+            event.preventDefault();
+            WorkItemFormService.getService().then((service) => service.beginSaveWorkItem($.noop, $.noop));
+        }
+    }
+});
 
 var control: Controller;
 
